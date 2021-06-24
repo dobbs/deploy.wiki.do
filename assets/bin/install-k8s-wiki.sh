2d0f038386cd25a3f4389aff3b18de26adc3bab7
Eric Dobbs <eric@dobbse.net>
2021-06-24 09:48:31 -0600

    ensure k8s v1.21 & update ingress config accordingly

--- assets/bin/install-k8s-wiki.sh
+++ assets/bin/install-k8s-wiki.sh
@@ -7,6 +7,7 @@ kubectl get svc/wiki-service >/dev/null 2>&1 || {
   k3d cluster create wiki \
       --k3s-server-arg --tls-san="127.0.0.1" \
       --port 80:80@loadbalancer \
+      --image rancher/k3s:v1.21.2-k3s1 \
       --volume "$HOME/.wiki-k8s:/macos/.wiki-k8s" \
       --volume "$HOME/workspace/fedwiki:/macos/fedwiki" \
       --volume "$PWD/assets/wiki:/var/lib/rancher/k3s/server/manifests/wiki" \
